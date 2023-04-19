3848f4acba4f8e104b65bce00088f6a0c6b4a3cc
Valeri Karkashadze <118384756+valerikarakashadze@users.noreply.github.com>
2023-04-19 19:28:41 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,164 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
+ // ----------- cursor
+
+ const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+  cursor.setAttribute("data-fromTop", cursor.offsetTop - scrollY);
+  // console.log(e)
+});
+window.addEventListener("scroll", () => {
+  const fromTop = cursor.getAttribute("data-fromTop");
+  cursor.style.top = scrollY + parseInt(fromTop) + "px";
+  console.log(scrollY);
+});
+window.addEventListener("click", () => {
+  if (cursor.classList.contains("click")) {
+    cursor.classList.remove("click");
+    void cursor.offsetWidth; // trigger a DOM reflow
+    cursor.classList.add("click");
+  } else {
+    cursor.classList.add("click");
+  }
+});
+
+//  registration
+const form = document.getElementById('registration-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting normally
+
+  // check if all required fields are filled
+  const inputs = form.querySelectorAll('input[required], select[required]');
+  const filled = Array.from(inputs).every((input) => input.value.trim() !== '');
+
+  if (filled) {
+    // create success message
+    const alert = document.createElement('div');
+    alert.textContent = 'თქვენ წარმატებით გარიარეთ რეგისტრაცია';
+    alert.classList.add('alert', 'alert-success');
+    form.appendChild(alert);
+  } else {
+    // create error message
+    const alert = document.createElement('div');
+    alert.textContent = 'გთხოვთ შეავსოთ სარეგისტრაციო ფორმა';
+    alert.classList.add('alert', 'alert-danger');
+    form.appendChild(alert);
+  }
+});
+
+const input = document.getElementById('my-input');
+input.addEventListener('invalid', () => {
+  input.setCustomValidity('გთხოვთ შეავსოთ ველი');
+});
+
+
